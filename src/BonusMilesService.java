1177eba79d4d433a7eaeb40e2ad5833df5243444
Владислав Маслов <maslov-vd.inc@msk.elara.ru>
2023-03-09 08:53:21 +0300

    Доработка BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,7 @@
 public class BonusMilesService {
     public int calculate(int cost) {
-        int forBonus = 20;
-        int cost = 1200;
-        int result = cost / forBonus;
-        return result;
+
+        return cost / 20;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10000;
+        int price = 20000;
         int miles = service.calculate(price);
         System.out.println("Начислено " + miles + " миль(и)");
     }
